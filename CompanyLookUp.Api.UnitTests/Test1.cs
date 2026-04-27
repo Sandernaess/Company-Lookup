e29676f27d52af3c81b791a9416a7d7f91001839
Sandernaess <Sander.naess@hotmail.com>
2026-04-27 22:11:50 +0200

    Add project files.

---
+++ CompanyLookUp.Api.UnitTests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace CompanyLookUp.Api.UnitTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
